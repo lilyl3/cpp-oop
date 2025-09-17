32813259e82d5dd59831f999ebf5d3c11a020784
lilyl3 <lily.lin.915@gmail.com>
2025-09-17 15:56:25 -0700

    Define different versions of HashFunction

--- bloom-filter/include/hash_function.h
+++ bloom-filter/include/hash_function.h
@@ -1,10 +1,41 @@
 #ifndef HASH_FUNCTION_H
 #define HASH_FUNCTION_H
 
+#include <functional>
+
 class HashFunction {
+    protected:
+        int m;  // size of BitArray
     public:
+        HashFunction(int m) : m(m) {}
         virtual ~HashFunction() = default;
         virtual int hash(int element) = 0;
+};
+
+class ModularHash : public HashFunction {
+    public:
+        ModularHash(int m) : HashFunction(m) {}
+        int hash(int element) override {
+            return element % m;
+        }
+};
+
+class MultiplicativeHash : public HashFunction {
+    private:
+        int a, b;
+    public:
+        MultiplicativeHash(int m, int a, int b) : HashFunction(m), a(a), b(b) {}
+        int hash(int element) override {
+            return (a * element + b) % m;
+        }
+};
+
+class StdHash : public HashFunction {
+    public:
+        StdHash(int m) : HashFunction(m) {}
+        int hash(int element) override {
+            return std::hash<int>{} (element) % m;
+        }
 };
 
 #endif
